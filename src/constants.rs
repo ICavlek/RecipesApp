a40f1150161cb4c88ecfb958c7bf453e90e4d4e0
ICavlek <ivan.cavlek1403@gmail.com>
2022-12-19 20:32:23 +0100

    Added unit test for const STORAGE_FILE_PATH - cargo test

--- src/constants.rs
+++ src/constants.rs
@@ -10,3 +10,13 @@ pub const STORAGE_FILE_PATH: &str = "./recipes.json";
 pub static KEYS: Lazy<identity::Keypair> = Lazy::new(|| identity::Keypair::generate_ed25519());
 pub static PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(KEYS.public()));
 pub static TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("recipes"));
+
+#[cfg(test)]
+mod tests {
+    use crate::constants::STORAGE_FILE_PATH;
+
+    #[test]
+    fn test_storage_file_path() {
+        assert_eq!(STORAGE_FILE_PATH, "./recipes.json");
+    }
+}
