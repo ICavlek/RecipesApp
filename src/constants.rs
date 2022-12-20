93c9e2eab4ffb817ea3fdbbc464cf251cba012d0
ICavlek <ivan.cavlek1403@gmail.com>
2022-12-20 16:22:13 +0100

    Updated Rust version and documentation

--- src/constants.rs
+++ src/constants.rs
@@ -1,14 +1,14 @@
 use libp2p::{floodsub::Topic, identity, PeerId};
 use once_cell::sync::Lazy;
 
-/// Constants Title
-///
-/// # Constants
-///
-/// Constants description
+/// # Path to the recipes
+/// Located in hardcoded path
 pub const STORAGE_FILE_PATH: &str = "./recipes.json";
+/// # Keys constant
 pub static KEYS: Lazy<identity::Keypair> = Lazy::new(|| identity::Keypair::generate_ed25519());
+/// # Peer Id constant
 pub static PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(KEYS.public()));
+/// # Topic constant
 pub static TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("recipes"));
 
 #[cfg(test)]
