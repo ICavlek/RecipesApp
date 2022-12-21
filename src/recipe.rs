0d27c2099687321de60f41e4b761c52d9717597e
ICavlek <ivan.cavlek1403@gmail.com>
2022-12-21 11:21:23 +0100

    Removed unnecessary stuff for now - Test SUCCESS

--- src/recipe.rs
+++ src/recipe.rs
@@ -4,14 +4,12 @@ use libp2p::{
     swarm::NetworkBehaviourEventProcess,
     NetworkBehaviour, PeerId,
 };
-use log::{error, info};
 use serde::{Deserialize, Serialize};
-use tokio::{fs, sync::mpsc};
+use tokio::sync::mpsc;
 
-use crate::messages::{ListMode, ListRequest, ListResponse};
+use crate::messages::ListResponse;
 
 pub type Recipes = Vec<Recipe>;
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Recipe {
@@ -33,114 +31,18 @@ pub struct RecipeBehaviour {
 }
 
 impl NetworkBehaviourEventProcess<FloodsubEvent> for RecipeBehaviour {
-    fn inject_event(&mut self, event: FloodsubEvent) {
-        match event {
-            FloodsubEvent::Message(msg) => {
-                if let Ok(resp) = serde_json::from_slice::<ListResponse>(&msg.data) {
-                    if resp.receiver == self.peer_id.to_string() {
-                        info!("Response from {}:", msg.source);
-                        resp.data.iter().for_each(|r| info!("{:?}", r));
-                    }
-                } else if let Ok(req) = serde_json::from_slice::<ListRequest>(&msg.data) {
-                    match req.mode {
-                        ListMode::ALL => {
-                            info!("Received ALL req: {:?} from {:?}", req, msg.source);
-                            respond_with_public_recipes(
-                                self.response_sender.clone(),
-                                msg.source.to_string(),
-                            );
-                        }
-                        ListMode::One(ref peer_id) => {
-                            if peer_id == &self.peer_id.to_string() {
-                                info!("Received req: {:?} from {:?}", req, msg.source);
-                                respond_with_public_recipes(
-                                    self.response_sender.clone(),
-                                    msg.source.to_string(),
-                                );
-                            }
-                        }
-                    }
-                }
-            }
-            _ => (),
-        }
-    }
-}
-
-fn respond_with_public_recipes(sender: mpsc::UnboundedSender<ListResponse>, receiver: String) {
-    tokio::spawn(async move {
-        match read_local_recipes().await {
-            Ok(recipes) => {
-                let resp = ListResponse {
-                    mode: ListMode::ALL,
-                    receiver,
-                    data: recipes.into_iter().filter(|r| r.public).collect(),
-                };
-                if let Err(e) = sender.send(resp) {
-                    error!("error sending response via channel, {}", e);
-                }
-            }
-            Err(e) => error!("error fetching local recipes to answer ALL request, {}", e),
-        }
-    });
-}
-
-async fn read_local_recipes() -> Result<Recipes> {
-    const STORAGE_FILE_PATH: &str = "./recipes.json";
-    let content = fs::read(STORAGE_FILE_PATH).await?;
-    let result = serde_json::from_slice(&content)?;
-    Ok(result)
+    fn inject_event(&mut self, _: FloodsubEvent) {}
 }
 
 impl NetworkBehaviourEventProcess<MdnsEvent> for RecipeBehaviour {
-    fn inject_event(&mut self, event: MdnsEvent) {
-        match event {
-            MdnsEvent::Discovered(discovered_list) => {
-                for (peer, _addr) in discovered_list {
-                    self.floodsub.add_node_to_partial_view(peer);
-                }
-            }
-            MdnsEvent::Expired(expired_list) => {
-                for (peer, _addr) in expired_list {
-                    if !self.mdns.has_node(&peer) {
-                        self.floodsub.remove_node_from_partial_view(&peer);
-                    }
-                }
-            }
-        }
-    }
+    fn inject_event(&mut self, _: MdnsEvent) {}
 }
 
 #[cfg(test)]
 mod tests {
-    use libp2p::{
-        floodsub::{Floodsub, Topic},
-        identity,
-        mdns::Mdns,
-        PeerId,
-    };
-    use tokio::sync::mpsc;
-
-    use crate::{messages::ListResponse, recipe::RecipeBehaviour};
 
     #[tokio::test]
     async fn test_recipe() {
-        let keys: identity::Keypair = identity::Keypair::generate_ed25519();
-        let peer_id: PeerId = PeerId::from(keys.public());
-        let topic: Topic = Topic::new("recipes");
-
-        let (response_sender, _) = mpsc::unbounded_channel::<ListResponse>();
-
-        let mut behaviour = RecipeBehaviour {
-            floodsub: Floodsub::new(peer_id),
-            mdns: Mdns::new(Default::default())
-                .await
-                .expect("can create mdns"),
-            response_sender: response_sender,
-            peer_id: peer_id,
-        };
-        println!("{}", peer_id);
-        behaviour.floodsub.subscribe(topic);
         assert_eq!(1, 1);
     }
 }
