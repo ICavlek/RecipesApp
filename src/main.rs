8a679fda1b8d662df093c818b0583e04fda88d6d
ICavlek <ivan.cavlek1403@gmail.com>
2023-01-04 21:32:12 +0100

    Only one account per application

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,10 @@ mod server;
 use client::*;
 use server::*;
 
-use std::thread;
-
 /// Main function
-#[tokio::main]
-async fn main() {
+fn main() {
     pretty_env_logger::init();
-
-    let thread1 = thread::spawn(|| {
-        let john = Client::new();
-        let mut server = Server::new(john);
-        server.start();
-    });
-    let thread2 = thread::spawn(|| {
-        let mark = Client::new();
-        let mut server = Server::new(mark);
-        server.start();
-    });
-    let thread3 = thread::spawn(|| {
-        let bruce = Client::new();
-        let mut server = Server::new(bruce);
-        server.start();
-    });
-    thread1.join().unwrap();
-    thread2.join().unwrap();
-    thread3.join().unwrap();
+    let john = Client::new();
+    let mut server = Server::new(john);
+    server.start();
 }
