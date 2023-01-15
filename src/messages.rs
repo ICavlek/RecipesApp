3c79c218e80990e6fa7ae5bd309ae1a04154e13e
ICavlek <ivan.cavlek1403@gmail.com>
2023-01-15 15:51:40 +0100

    Adapted NetowrkBehavoueEvenProcesses

--- src/messages.rs
+++ src/messages.rs
@@ -1,5 +1,8 @@
 use serde::{Deserialize, Serialize};
 
+use crate::recipe::Recipe;
+type Recipes = Vec<Recipe>;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub enum ListMode {
     ALL,
@@ -9,6 +12,7 @@ pub enum ListMode {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ListResponse {
     pub mode: ListMode,
+    pub data: Recipes,
     pub receiver: String,
 }
 
--- src/recipe.rs
+++ src/recipe.rs
@@ -4,10 +4,11 @@ use libp2p::{
     swarm::NetworkBehaviourEventProcess,
     NetworkBehaviour, PeerId,
 };
+use log::info;
 use serde::{Deserialize, Serialize};
 use tokio::sync::mpsc;
 
-use crate::messages::ListResponse;
+use crate::messages::{ListMode, ListRequest, ListResponse};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Recipe {
@@ -29,11 +30,57 @@ pub struct RecipeBehaviour {
 }
 
 impl NetworkBehaviourEventProcess<FloodsubEvent> for RecipeBehaviour {
-    fn inject_event(&mut self, _: FloodsubEvent) {}
+    fn inject_event(&mut self, event: FloodsubEvent) {
+        match event {
+            FloodsubEvent::Message(msg) => {
+                if let Ok(resp) = serde_json::from_slice::<ListResponse>(&msg.data) {
+                    if resp.receiver == self.peer_id.to_string() {
+                        info!("Response from {}:", msg.source);
+                        resp.data.iter().for_each(|r| info!("{:?}", r));
+                    }
+                } else if let Ok(req) = serde_json::from_slice::<ListRequest>(&msg.data) {
+                    match req.mode {
+                        ListMode::ALL => {
+                            info!("Received ALL req: {:?} from {:?}", req, msg.source);
+                            // respond_with_public_recipes(
+                            //     self.response_sender.clone(),
+                            //     msg.source.to_string(),
+                            // );
+                        }
+                        ListMode::One(ref peer_id) => {
+                            if peer_id == &self.peer_id.to_string() {
+                                info!("Received req: {:?} from {:?}", req, msg.source);
+                                // respond_with_public_recipes(
+                                //     self.response_sender.clone(),
+                                //     msg.source.to_string(),
+                                // );
+                            }
+                        }
+                    }
+                }
+            }
+            _ => (),
+        }
+    }
 }
 
 impl NetworkBehaviourEventProcess<MdnsEvent> for RecipeBehaviour {
-    fn inject_event(&mut self, _: MdnsEvent) {}
+    fn inject_event(&mut self, event: MdnsEvent) {
+        match event {
+            MdnsEvent::Discovered(discovered_list) => {
+                for (peer, _addr) in discovered_list {
+                    self.floodsub.add_node_to_partial_view(peer);
+                }
+            }
+            MdnsEvent::Expired(expired_list) => {
+                for (peer, _addr) in expired_list {
+                    if !self.mdns.has_node(&peer) {
+                        self.floodsub.remove_node_from_partial_view(&peer);
+                    }
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
